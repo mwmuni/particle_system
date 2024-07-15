b0fc94a91b0c2b53c021774ab186d39ff09871d1
Matthew Muller <matthew.muller@uon.edu.au>
2024-07-15 22:29:25 +0800

    Added more windows handling and thread support
    Signed-off-by: Matthew Muller <matthew.muller@uon.edu.au>

--- particle_system.c
+++ particle_system.c
@@ -1,27 +1,29 @@
 #define SDL_MAIN_HANDLED
 
-// if you are using SDL2, you should include SDL2/SDL.h on linux, and SDL.h on windows
+// Include the appropriate SDL header based on the platform
 #ifdef _WIN32
 #include <SDL.h>
-#endif
-#ifdef __linux__
+#include <wtypes.h>
+#include <windows.h>
+#include <process.h>
+#else
 #include <SDL2/SDL.h>
+#include <pthread.h>
 #endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
 
 #define SIMULATION_RANGE 5.0f
-#define NUM_PARTICLES 200
-// #define WINDOW_WIDTH 800
-// #define WINDOW_HEIGHT 600
+#define NUM_PARTICLES 2000
 #define WINDOW_WIDTH 1920
 #define WINDOW_HEIGHT 1080
 #define REPULSION_STRENGTH 0.3f
 #define MIN_DISTANCE 0.1f
 #define BASE_PARTICLE_SIZE 1.0f
-
+#define NUM_THREADS 32
 
 typedef struct {
     float x, y;
@@ -29,6 +31,13 @@ typedef struct {
     float radius;
     SDL_Color color;
 } Particle;
+
+typedef struct {
+    Particle* particles;
+    int start;
+    int end;
+    float dt;
+} ThreadData;
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
@@ -52,14 +61,13 @@ void init_sdl() {
     }
 }
 
-// Update the particle initialization
 void init_particles(Particle* particles) {
     for (int i = 0; i < NUM_PARTICLES; i++) {
         particles[i].x = ((float)rand() / RAND_MAX) * SIMULATION_RANGE * 2 - SIMULATION_RANGE;
         particles[i].y = ((float)rand() / RAND_MAX) * SIMULATION_RANGE * 2 - SIMULATION_RANGE;
-        particles[i].vx = ((float)rand() / RAND_MAX) * 1 - 0.5;  // Reduced velocity range
-        particles[i].vy = ((float)rand() / RAND_MAX) * 1 - 0.5;  // Reduced velocity range
-        particles[i].radius = BASE_PARTICLE_SIZE;  // Increased particle size for visibility
+        particles[i].vx = ((float)rand() / RAND_MAX) * 1 - 0.5;
+        particles[i].vy = ((float)rand() / RAND_MAX) * 1 - 0.5;
+        particles[i].radius = BASE_PARTICLE_SIZE;
         particles[i].color.r = rand() % 256;
         particles[i].color.g = rand() % 256;
         particles[i].color.b = rand() % 256;
@@ -67,21 +75,25 @@ void init_particles(Particle* particles) {
     }
 }
 
-// Update the particle update function
-void update_particles(Particle* particles, float dt) {
-    for (int i = 0; i < NUM_PARTICLES; i++) {
+void* update_particles_thread(void* arg) {
+    ThreadData* data = (ThreadData*)arg;
+    Particle* particles = data->particles;
+    int start = data->start;
+    int end = data->end;
+    float dt = data->dt;
+
+    for (int i = start; i < end; i++) {
         float total_force_x = 0;
         float total_force_y = 0;
 
-        // Calculate repulsion forces from other particles
         for (int j = 0; j < NUM_PARTICLES; j++) {
             if (i != j) {
                 float dx = particles[i].x - particles[j].x;
                 float dy = particles[i].y - particles[j].y;
-                float distance = sqrt(dx*dx + dy*dy);
+                float distance = sqrt(dx * dx + dy * dy);
 
                 if (distance < MIN_DISTANCE) {
-                    distance = MIN_DISTANCE;  // Prevent division by zero
+                    distance = MIN_DISTANCE;
                 }
 
                 float force = REPULSION_STRENGTH / (distance * distance);
@@ -90,15 +102,11 @@ void update_particles(Particle* particles, float dt) {
             }
         }
 
-        // Update velocity based on repulsion forces
         particles[i].vx += total_force_x * dt;
         particles[i].vy += total_force_y * dt;
-
-        // Update position
         particles[i].x += particles[i].vx * dt;
         particles[i].y += particles[i].vy * dt;
 
-        // Bounce off walls (keep this part from the original function)
         if (particles[i].x < -SIMULATION_RANGE || particles[i].x > SIMULATION_RANGE) {
             particles[i].vx *= -1;
             particles[i].x = particles[i].x < 0 ? -SIMULATION_RANGE : SIMULATION_RANGE;
@@ -108,10 +116,43 @@ void update_particles(Particle* particles, float dt) {
             particles[i].y = particles[i].y < 0 ? -SIMULATION_RANGE : SIMULATION_RANGE;
         }
     }
+
+    return NULL;
 }
 
+void update_particles(Particle* particles, float dt) {
+#ifdef _WIN32
+    HANDLE threads[NUM_THREADS];
+#else
+    pthread_t threads[NUM_THREADS];
+#endif
+    ThreadData thread_data[NUM_THREADS];
 
-// Update the rendering function to map simulation coordinates to screen coordinates
+    int particles_per_thread = NUM_PARTICLES / NUM_THREADS;
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_data[i].particles = particles;
+        thread_data[i].start = i * particles_per_thread;
+        thread_data[i].end = (i == NUM_THREADS - 1) ? NUM_PARTICLES : (i + 1) * particles_per_thread;
+        thread_data[i].dt = dt;
+
+#ifdef _WIN32
+        threads[i] = (HANDLE)_beginthreadex(NULL, 0, (unsigned(__stdcall*)(void*))update_particles_thread, &thread_data[i], 0, NULL);
+#else
+        pthread_create(&threads[i], NULL, update_particles_thread, &thread_data[i]);
+#endif
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+#ifdef _WIN32
+        WaitForSingleObject(threads[i], INFINITE);
+        CloseHandle(threads[i]);
+#else
+        pthread_join(threads[i], NULL);
+#endif
+    }
+}
+
 void render_particles(SDL_Renderer* renderer, Particle* particles) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
@@ -119,7 +160,6 @@ void render_particles(SDL_Renderer* renderer, Particle* particles) {
     for (int i = 0; i < NUM_PARTICLES; i++) {
         SDL_SetRenderDrawColor(renderer, particles[i].color.r, particles[i].color.g, particles[i].color.b, particles[i].color.a);
         
-        // Map simulation coordinates to screen coordinates
         int screen_x = (int)((particles[i].x + SIMULATION_RANGE) / (2 * SIMULATION_RANGE) * WINDOW_WIDTH);
         int screen_y = (int)((particles[i].y + SIMULATION_RANGE) / (2 * SIMULATION_RANGE) * WINDOW_HEIGHT);
         
@@ -135,7 +175,12 @@ void render_particles(SDL_Renderer* renderer, Particle* particles) {
     SDL_RenderPresent(renderer);
 }
 
-int main(int argc, char* args[]) {
+#ifdef _WIN32
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#else
+int main(int argc, char* args[])
+#endif
+{
     srand(time(NULL));
     init_sdl();
     
@@ -161,7 +206,7 @@ int main(int argc, char* args[]) {
         render_particles(renderer, particles);
         
         Uint32 frame_time = SDL_GetTicks() - last_time;
-        const Uint32 target_frame_time = 1000 / 144; // Target frame time in milliseconds
+        const Uint32 target_frame_time = 1000 / 144;
 
         if (frame_time < target_frame_time) {
             Uint32 delay_time = target_frame_time - frame_time;
